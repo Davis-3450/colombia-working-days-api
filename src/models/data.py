b14efaf0369976d399625db2fbd2f90d43ad207f
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-28 18:59:33 -0500

    refactor: implement data models, add type adapter for holiday list

---
+++ src/models/data.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta, date
+
+from pydantic import BaseModel, Field, RootModel, TypeAdapter
+from typing import List
+from enum import Enum
+
+# datetime
+# class request(BaseModel):
+#     days: int | None = None
+#     hours: timedelta | None = None
+#     datetime: datetime | None = None
+
+
+class Error(Enum):
+    """
+    Error types
+    """
+    INVALID_PARAMETERS = "InvalidParameters"
+
+class ErrorMessage(Enum):
+    """
+    Error messages
+    """
+    INVALID_PARAMETERS = "Invalid parameters"
+
+
+holiday_list_adapter = TypeAdapter(List[date])
