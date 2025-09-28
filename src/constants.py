0c46d027106e8a5f4570e8e40fbac208d0c0e9d6
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-28 03:53:17 -0500

    feat: add constants for holidays and work hours for convenience

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+from zoneinfo import ZoneInfo
+
+HOLIDAYS = "https://content.capta.co/Recruitment/WorkingDays.json"
+TZ = ZoneInfo("America/Bogota")
+WORK_HOUR_START = 8
+WORK_HOUR_END = 17
+LUNCH_HOUR_START = 12
+LUNCH_HOUR_END = 13
