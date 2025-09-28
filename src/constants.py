40f4522133cc19037e79a28bc96be0221bafc179
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-28 04:10:20 -0500

    addd holiday list constant

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,16 @@
 from zoneinfo import ZoneInfo
 
+import httpx
+
 HOLIDAYS = "https://content.capta.co/Recruitment/WorkingDays.json"
 TZ = ZoneInfo("America/Bogota")
 WORK_HOUR_START = 8
 WORK_HOUR_END = 17
 LUNCH_HOUR_START = 12
 LUNCH_HOUR_END = 13
+
+# Note: this is an oversimplification, only happens on cold boot, I doubt we need amything else...
+HOLIDAYS_LIST: list = []
+with httpx.Client() as client:
+    r = client.get(HOLIDAYS)
+    HOLIDAYS_LIST = r.json()
