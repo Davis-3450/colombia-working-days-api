29401281ed48b8eaf5ad02b6944f74bff4bf477d
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-28 18:12:06 -0500

    refactor: rename HOLIDAYS constant to HOLIDAYS_URL, add type adapter for holiday list

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,21 @@
+from typing import List
 from zoneinfo import ZoneInfo
-
 import httpx
+from datetime import date
+from pydantic import TypeAdapter
 
-HOLIDAYS = "https://content.capta.co/Recruitment/WorkingDays.json"
+HOLIDAYS_URL = "https://content.capta.co/Recruitment/WorkingDays.json"
 TZ = ZoneInfo("America/Bogota")
 WORK_HOUR_START = 8
 WORK_HOUR_END = 17
 LUNCH_HOUR_START = 12
 LUNCH_HOUR_END = 13
 
+holyday_list_adapter: TypeAdapter[List[date]] = TypeAdapter(List[date])
+
 # Note: this is an oversimplification, only happens on cold boot, I doubt we need amything else...
-HOLIDAYS_LIST: list = []
+# TODO: fix, this might run on each import.
+holyday_list = holyday_list_adapter.validate_python([])
 with httpx.Client() as client:
-    r = client.get(HOLIDAYS)
-    HOLIDAYS_LIST = r.json()
+    r = client.get(HOLIDAYS_URL)
+    holyday_list = holyday_list_adapter.validate_python(r.json())
