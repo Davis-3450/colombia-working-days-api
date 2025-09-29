7917e032f369a4f608047b2dac513ee0725ddbcb
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-09-28 19:04:47 -0500

    refactor: remove old logic and imports

--- src/main.py
+++ src/main.py
@@ -1,19 +1,11 @@
 from datetime import datetime
-import os
 
-import httpx
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from models.responses import InvalidResponse, Response
 from constants import holyday_list
 
 # error: { "error": "InvalidParameters", "message": "Detalle del error" }
-
-# Note we could use a cron job to update the holidays, calculating on each request is stupid, for now lets do it on cold boot
-with httpx.Client() as client:
-    r = client.get(HOLIDAYS)
-    working_days = r.json()
-
 
 app = FastAPI()
 
