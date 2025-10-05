77f2ea79630bf448aa55e04ae308c418bb84cef5
Moshi <64158093+Davis-3450@users.noreply.github.com>
2025-10-05 03:28:03 -0500

    fix: update holiday loading logic

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,15 @@
+from datetime import date, datetime
 from enum import Enum
-from typing import List
 from zoneinfo import ZoneInfo
+
 import httpx
-from datetime import date
-from pydantic import TypeAdapter
-from models.data import holyday_list_adapter
+
+from src.models.data import holyday_list_adapter
 
 HOLIDAYS_URL = "https://content.capta.co/Recruitment/WorkingDays.json"
 TZ = ZoneInfo("America/Bogota")
+
+
 class Weekday(int, Enum):
     MONDAY = 0
     TUESDAY = 1
@@ -17,15 +19,20 @@ class Weekday(int, Enum):
     SATURDAY = 5
     SUNDAY = 6
 
+
 class WorkHour(int, Enum):
     WORK_START = 8
     LUNCH_START = 12
     LUNCH_END = 13
     WORK_END = 17
 
+
 # Note: this is an oversimplification, only happens on cold boot, I doubt we need amything else...
 # TODO: fix, this might run on each import.
 holyday_list = holyday_list_adapter.validate_python([])
 with httpx.Client() as client:
     r = client.get(HOLIDAYS_URL)
-    holyday_list = holyday_list_adapter.validate_python(r.json())
+    for date in r.json():
+        date = datetime.strptime(date, "%Y-%m-%d").astimezone(TZ)
+        holyday_list.append(date)
+    holyday_list = holyday_list_adapter.validate_python(holyday_list)
